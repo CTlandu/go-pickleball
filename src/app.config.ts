09c1a0f8a4d7ebc739bec384b676c70e938d11ea
CTlandu <jizhoutang@outlook.com>
2025-03-02 00:32:49 -0500

    Work on Drill Page, 添加了四个卡片样式

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,34 +1,34 @@
 export default defineAppConfig({
   pages: [
-    "pages/drill/index",
-    "pages/ranking/index",
-    "pages/hangout/index",
-    "pages/index/index",
+    'pages/drill/index',  // 将 drill 页面设为首页
+    'pages/ranking/index',
+    'pages/hangout/index',
+    'pages/index/index'   // 将 index 页面放到最后
   ],
   tabBar: {
-    color: "#999999",
-    selectedColor: "#1AAD19",
-    backgroundColor: "#ffffff",
-    borderStyle: "black",
+    color: '#999999',
+    selectedColor: '#1AAD19',
+    backgroundColor: '#ffffff',
+    borderStyle: 'black',
     list: [
       {
-        pagePath: "pages/drill/index",
-        text: "练球",
+        pagePath: 'pages/drill/index',
+        text: '练球'
       },
       {
-        pagePath: "pages/ranking/index",
-        text: "排名",
+        pagePath: 'pages/ranking/index',
+        text: '排名'
       },
       {
-        pagePath: "pages/hangout/index",
-        text: "约球",
-      },
-    ],
+        pagePath: 'pages/hangout/index',
+        text: '约球'
+      }
+    ]
   },
   window: {
-    backgroundTextStyle: "light",
-    navigationBarBackgroundColor: "#fff",
-    navigationBarTitleText: "Go Pickleball",
-    navigationBarTextStyle: "black",
-  },
-});
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'Go Pickleball',
+    navigationBarTextStyle: 'black'
+  }
+})
--- src/pages/drill/index.tsx
+++ src/pages/drill/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from '@tarojs/components'
+import { View, Text, Image } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
 
@@ -7,9 +7,32 @@ export default function Drill() {
     console.log('Drill page loaded.')
   })
 
+  // 练球类型数据
+  const drillTypes = [
+    { id: 1, title: 'Dink', subtitle: '丁克球', description: '短距离控制性击球练习' },
+    { id: 2, title: 'Drop', subtitle: '长丁克', description: '从后场到前场的控制性击球' },
+    { id: 3, title: 'Volley', subtitle: '截击', description: '网前截击球技术练习' },
+    { id: 4, title: 'Wall Drill', subtitle: '单人对墙练习', description: '利用墙壁进行的单人练习' }
+  ]
+
   return (
-    <View className='drill'>
-      <Text>练球页面</Text>
+    <View className='drill-page'>
+      <View className='drill-container'>
+        {drillTypes.map(drill => (
+          <View key={drill.id} className='drill-card'>
+            <Image 
+              className='drill-image' 
+              src='https://via.placeholder.com/150' 
+              mode='aspectFill'
+            />
+            <View className='drill-info'>
+              <Text className='drill-title'>{drill.title}</Text>
+              <Text className='drill-subtitle'>{drill.subtitle}</Text>
+              <Text className='drill-description'>{drill.description}</Text>
+            </View>
+          </View>
+        ))}
+      </View>
     </View>
   )
 }
